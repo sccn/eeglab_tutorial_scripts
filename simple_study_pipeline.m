f137a2e36d3afa58068a9c664eb83038a82dcd45
Arnaud Delorme <arnodelorme@gmail.com>
2022-08-01 16:02:01 -0700

    only 2 subjects

--- simple_study_pipeline.m
+++ simple_study_pipeline.m
@@ -1,5 +1,5 @@
 % check folder
-eeglab
+eeglab;
 if ~exist('task-P300_events.json', 'file')
     error('Download the data from https://openneuro.org/datasets/ds003061/ and go to the downloaded folder');
 else
@@ -7,42 +7,42 @@ else
 end
 
 % import data
-[STUDY, ALLEEG] = pop_importbids(filepath, 'studyName','Oddball');
-EEG = ALLEEG; CURRENTSET = 1:length(EEG); CURRENTSTUDY = 1; eeglab redraw; % redraw EEGLAB interface (optional)
+pop_editoptions( 'option_storedisk', 1); % only one dataset in memory at a time
+[STUDY, ALLEEG] = pop_importbids(filepath, 'studyName','Oddball', 'subjects', [1:2]);
 
-% remove non-EEG channels (it is also possible to process EEG data with non-EEG data
-EEG = pop_select( EEG,'nochannel',{'EXG1','EXG2','EXG3','EXG4','EXG5','EXG8','Resp'});
+% remove non-ALLEEG channels (it is also possible to process ALLEEG data with non-ALLEEG data
+ALLEEG = pop_select( ALLEEG,'nochannel',{'EXG1','EXG2','EXG3','EXG4','EXG5','EXG6','EXG7','EXG8', 'GSR1', 'GSR2', 'Erg1', 'Erg2', 'Resp', 'Plet', 'Temp'});
 
 % compute average reference
-EEG = pop_reref( EEG,[]);
+ALLEEG = pop_reref( ALLEEG,[]);
 
 % clean data using the clean_rawdata plugin
-EEG = clean_artifacts( EEG,'FlatlineCriterion',5,'ChannelCriterion',0.8, ...
+ALLEEG = pop_clean_rawdata( ALLEEG,'FlatlineCriterion',5,'ChannelCriterion',0.85, ...
     'LineNoiseCriterion',4,'Highpass',[0.25 0.75] ,'BurstCriterion',20, ...
     'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian', ...
     'WindowCriterionTolerances',[-Inf 7] ,'fusechanrej',1);
 
 % recompute average reference interpolating missing channels (and removing
 % them again after average reference - STUDY functions handle them automatically)
-EEG = pop_reref( EEG,[],'interpchan',[]);
+ALLEEG = pop_reref( ALLEEG,[],'interpchan',[]);
 
 % run ICA reducing the dimention by 1 to account for average reference 
-EEG = pop_runica(EEG, 'icatype','runica','concatcond','on','options',{'pca',-1});
+ALLEEG = pop_runica(ALLEEG, 'icatype','runica','concatcond','on','options',{'pca',-1});
 
 % run ICLabel and flag artifactual components
-EEG = pop_iclabel(EEG, 'default');
-EEG = pop_icflag( EEG,[NaN NaN;0.9 1;0.9 1;NaN NaN;NaN NaN;NaN NaN;NaN NaN]);
+ALLEEG = pop_iclabel(ALLEEG, 'default');
+ALLEEG = pop_icflag( ALLEEG,[NaN NaN;0.9 1;0.9 1;NaN NaN;NaN NaN;NaN NaN;NaN NaN]);
 
 % extract data epochs
-EEG = pop_epoch( EEG,{'oddball_with_reponse','standard'},[-1 2] ,'epochinfo','yes');
-EEG = eeg_checkset( EEG );
-EEG = pop_rmbase( EEG,[-1000 0] ,[]);
+ALLEEG = pop_epoch( ALLEEG,{'oddball_with_reponse','standard'},[-1 2] ,'epochinfo','yes');
+ALLEEG = eeg_checkset( ALLEEG );
+ALLEEG = pop_rmbase( ALLEEG,[-1000 0] ,[]);
 
 % create STUDY design
-ALLEEG = EEG;
+STUDY = std_maketrialinfo(STUDY, ALLEEG);
 STUDY = std_makedesign(STUDY, ALLEEG, 1, 'name','STUDY.design 1','delfiles','off', ...
     'defaultdesign','off','variable1','type','values1',{'oddball_with_reponse','standard'},...
-    'vartype1','categorical','subjselect',{'sub-001'});
+    'vartype1','categorical','subjselect', STUDY.subject);
 
 % precompute ERPs at the STUDY level
 [STUDY, ALLEEG] = std_precomp(STUDY, ALLEEG, {},'savetrials','on','rmicacomps','on','interp','on','recompute','on','erp','on');
@@ -51,3 +51,6 @@ STUDY = std_makedesign(STUDY, ALLEEG, 1, 'name','STUDY.design 1','delfiles','off
 STUDY = pop_erpparams(STUDY, 'topotime',350);
 chanlocs = eeg_mergelocs(ALLEEG.chanlocs); % get all channels from all datasets
 STUDY = std_erpplot(STUDY,ALLEEG,'channels', {chanlocs.labels}, 'design', 1);
+
+% revert default option
+pop_editoptions( 'option_storedisk', 0);
